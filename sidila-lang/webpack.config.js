592584141758117fc089c015f6e7ad3c39f457da
Leandro <drkbugs@gmail.com>
2025-01-04 22:43:29 -0300

    eval-source-map

--- sidila-lang/webpack.config.js
+++ sidila-lang/webpack.config.js
@@ -2,6 +2,7 @@ var path = require('path');
 
 module.exports = {
   entry: './src/app.js',
+  devtool: 'eval-source-map',
   output: {
     path: path.resolve(__dirname),
     filename: '../site/static/sidila/peg.js',
